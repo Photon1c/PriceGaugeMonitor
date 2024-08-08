5b6b6a29af7a4b9ba3b0d50e347cb172df1c4c36
Leslie Cuadra <Lescua@gmail.com>
2024-08-08 09:30:59 -0700

    Corrected JSON NaN error

--- app.py
+++ app.py
@@ -2,13 +2,22 @@ from flask import Flask, jsonify, render_template
 import os
 import pandas as pd
 import yfinance as yf
+from dotenv import load_dotenv
+import numpy as np
 
 app = Flask(__name__)
+load_dotenv()
 
 def get_latest_prices_yfinance(symbols):
-    # Fetch the latest stock prices using yfinance
     data = yf.download(tickers=symbols, period='1d', interval='1m')
-    latest_prices = {symbol: data['Close'][symbol][-1] for symbol in symbols if symbol in data['Close']}
+    latest_prices = {}
+    for symbol in symbols:
+        try:
+            latest_price = data['Close'][symbol][-1]
+            # Replace NaN values with None
+            latest_prices[symbol] = None if np.isnan(latest_price) else latest_price
+        except KeyError:
+            latest_prices[symbol] = None
     return latest_prices
 
 @app.route('/')
